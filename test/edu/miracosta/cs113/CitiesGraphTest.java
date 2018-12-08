26de2fdd4560991eddcf01e7e6b68aedb1968218
Jesse Wolf <jessewolf42892@gmail.com>
2018-12-07 16:53:11 -0800

    Created ListGraph within CitiesGraph and tester to make sure it reads from file.

--- test/edu/miracosta/cs113/CitiesGraphTest.java
+++ test/edu/miracosta/cs113/CitiesGraphTest.java
@@ -1,4 +1,8 @@
 package edu.miracosta.cs113;
 
 public class CitiesGraphTest {
+    public static void main(String[] args) {
+        CitiesGraph test = new CitiesGraph();
+        System.out.println(test.toString());
+    }
 }
