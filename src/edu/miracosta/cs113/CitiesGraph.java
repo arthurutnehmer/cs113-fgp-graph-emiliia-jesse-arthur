c7ed5f87dc7b94ca6748a692b5a314651f145b01
Unknown <jessewolf42892@gmail.com>
2018-12-08 00:03:23 -0800

    Dijkstra's algorithm working fully within CitiesGraph class

--- src/edu/miracosta/cs113/CitiesGraph.java
+++ src/edu/miracosta/cs113/CitiesGraph.java
@@ -21,4 +21,52 @@ public class CitiesGraph {
         }
         citiesGraph = (ListGraph) ListGraph.createGraph(inputFile, false, "List");
     }
+    /**
+    /**
+    /**
+     * Dijkstra's Shortest-Path Algorithm
+     * @param graph The weighted graph to be searched
+     * @param start The start vertex
+     * @param pred Output array to contain the predecessors in the shortest path.
+     * @param dist Output array to contain the distance in the shortest path.
+     */
+    public void dijkstrasAlgorithm(Graph graph, int start, int[] pred, double[]dist) {
+        int numV = graph.getNumV();
+        HashSet<Integer> vMinusS = new HashSet<Integer>(numV);
+        //Initialize V-S
+        for(int i = 0; i < numV; i++) {
+            if(i != start) {
+                vMinusS.add(i);
+            }
+        }
+        //Initialize predecessor and distance.
+        for(int v : vMinusS) {
+            pred[v] = start;
+            dist[v] = graph.getEdge(start, v).getWeight();
+        }
+        //Main Loop
+        while(vMinusS.size() != 0) {
+            //Find the value u in V-S with the smallest dist[u]
+            double minDist = Double.POSITIVE_INFINITY;
+            int u = -1;
+            for(int v : vMinusS) {
+                if(dist[v] < minDist) {
+                    minDist = dist[v];
+                    u = v;
+                }
+            }
+            //Remove u from vMinusS
+            vMinusS.remove(u);
+            //Update the distances.
+            for(int v : vMinusS) {
+                if(graph.isEdge(u,v)) {
+                    double weight = graph.getEdge(u,v).getWeight();
+                    if(dist[u] + weight < dist[v]) {
+                        dist[v] = dist[u] + weight;
+                        pred[v] = u;
+                    }
+                }
+            }
+        }
+    }
 }
